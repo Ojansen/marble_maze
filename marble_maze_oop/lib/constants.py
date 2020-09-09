59093960b9a5f58b50e7bc217c1632422f39664d
Ojansen <jansen.obe@gmail.com>
2020-09-09 12:03:21 +0200

    lib folder and level selector added

---
+++ marble_maze_oop/lib/constants.py
@@ -0,0 +1,26 @@
+r = (255,0,0)
+b = (0,0,0)
+w = (255,255,255)
+g = (0,255,0)
+v = (30,80,30)
+
+x = 1
+y = 1
+
+maze1 = [[r, r, r, r, r, r, r, r],
+        [r, b, b, b, b, b, b, r],
+        [r, r, r, r, r, b, r, r],
+        [r, b, b, b, b, b, b, r],
+        [r, b, r, r, b, b, r, r],
+        [r, b, b, r, r, b, r, r],
+        [r, r, g, r, b, b, b, r],
+        [r, r, r, r, r, r, r, r]]
+
+maze2 = [[r, r, r, r, r, r, r, r],
+        [r, b, b, b, b, b, b, r],
+        [r, r, r, r, r, v, r, r],
+        [r, b, b, b, b, b, b, r],
+        [r, b, r, r, b, b, r, r],
+        [r, b, b, r, r, b, r, r],
+        [r, r, g, r, b, b, b, r],
+        [r, r, r, r, r, r, r, r]]
---
+++ marble_maze_oop/lib/send_score.py
@@ -0,0 +1,7 @@
+import requests
+
+
+def create_post(player_name, level, tries, score):
+    payload = {"Player_name": player_name, "level": level, "try": tries, "score": score}
+    r = requests.post('http://167.172.37.168/api/newscore', data=payload)
+    print(r.text)
